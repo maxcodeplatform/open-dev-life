091bd4a4350320201cf46dc0426a6c126868e4c1
Md.Sifat Hassan <hassansifat97@gmail.com>
2022-10-01 12:30:05 +0600

    Python solution of reverse linked list

---
+++ problem_solving/python/linked_list/reverse_linked_list.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        curr = head
+        prev = None
+
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
